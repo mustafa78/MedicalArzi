7c1c79456cd4e1b25518434bd7ecd2b2e3c71b0f
mkanchwa <mustafa.kanchwala@gmail.com>
2015-04-27 22:09:40 -0400

    Fixed the query and added the ibatis param.

--- src/com/example/medicalarzi/dao/SecurityMapper.java
+++ src/com/example/medicalarzi/dao/SecurityMapper.java
@@ -5,6 +5,8 @@ package com.example.medicalarzi.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.example.medicalarzi.model.SecurityRole;
 
 /**
@@ -13,8 +15,22 @@ import com.example.medicalarzi.model.SecurityRole;
  */
 public interface SecurityMapper {
 
+	/**
+	 * This method is responsible for returning all the possible security roles
+	 * for the patient.
+	 * 
+	 * @return
+	 */
 	public List<SecurityRole> selectAllRoles();
 
-	public List<SecurityRole> selectSecurityRolesForUser(Long ptntItsNumber);
+	/**
+	 * This method is responsible for getting all the security roles for the
+	 * patient.
+	 * 
+	 * @param ptntItsNumber
+	 * @return
+	 */
+	public List<SecurityRole> selectSecurityRolesForUser(
+			@Param("ptntItsNumber") Long ptntItsNumber);
 
 }
