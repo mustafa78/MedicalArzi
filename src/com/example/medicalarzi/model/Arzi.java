a0527e2ed41a39647c01c9c5dbc921c97b75a752
mkanchwa <mustafa.kanchwala@gmail.com>
2015-05-04 09:41:37 -0400

    Update arzi model class to support the dbase changes when creating a new arzi.

--- src/com/example/medicalarzi/model/Arzi.java
+++ src/com/example/medicalarzi/model/Arzi.java
@@ -3,123 +3,45 @@ package com.example.medicalarzi.model;
 import java.io.Serializable;
 import java.util.Date;
 
-public class Arzi implements Serializable {
+public class Arzi extends ArziHeader implements Serializable {
 
 	/**
 	 *
 	 */
 	private static final long serialVersionUID = -3071236194000826029L;
 
-	private Long arziId;
+	private Long reviewerItsNumber;
 
-	private Long itsNumber;
-	
-	private Long drItsNumber;
+	private GregHijDate reviewDate;
 
-	private Procedure procedure;
-
-	private Condition condition;
-
-	private Status status;
-
-	private ArziType arziType;
-	
-	private BodyPart bodyPart;
-	
-	private Date arziSubmitDate;
-	
-	private Date drReviewDate;
-
-	private String createdBy;
-
-	private Date createdDate;
-
-	private String updatedBy;
-
-	private Date updatedDate;
+	private GregHijDate statusChangeDate;
 
 	public Arzi() {
-		// TODO Auto-generated constructor stub
+		super();
 	}
 
-	public Long getArziId() {
-		return arziId;
-	}
-	
-	public void setArziId(Long arziId) {
-		this.arziId = arziId;
+	public Long getReviewerItsNumber() {
+		return reviewerItsNumber;
 	}
 
-	public Long getItsNumber() {
-		return itsNumber;
+	public void setReviewerItsNumber(Long reviewerItsNumber) {
+		this.reviewerItsNumber = reviewerItsNumber;
 	}
 
-	public void setItsNumber(Long itsNumber) {
-		this.itsNumber = itsNumber;
+	public GregHijDate getReviewDate() {
+		return reviewDate;
 	}
 
-	public Procedure getProcedure() {
-		return procedure;
+	public void setReviewDate(GregHijDate reviewDate) {
+		this.reviewDate = reviewDate;
 	}
 
-	public void setProcedure(Procedure procedure) {
-		this.procedure = procedure;
+	public GregHijDate getStatusChangeDate() {
+		return statusChangeDate;
 	}
 
-	public Condition getCondition() {
-		return condition;
-	}
-
-	public void setCondition(Condition condition) {
-		this.condition = condition;
-	}
-
-	public Status getStatus() {
-		return status;
-	}
-
-	public void setStatus(Status status) {
-		this.status = status;
-	}
-
-	public Long getDrItsNumber() {
-		return drItsNumber;
-	}
-
-	public void setDrItsNumber(Long drItsNumber) {
-		this.drItsNumber = drItsNumber;
-	}
-
-	public ArziType getArziType() {
-		return arziType;
-	}
-
-	public void setArziType(ArziType arziType) {
-		this.arziType = arziType;
-	}
-
-	public BodyPart getBodyPart() {
-		return bodyPart;
-	}
-
-	public void setBodyPart(BodyPart bodyPart) {
-		this.bodyPart = bodyPart;
-	}
-
-	public Date getArziSubmitDate() {
-		return arziSubmitDate;
-	}
-
-	public void setArziSubmitDate(Date arziSubmitDate) {
-		this.arziSubmitDate = arziSubmitDate;
-	}
-
-	public Date getDrReviewDate() {
-		return drReviewDate;
-	}
-
-	public void setDrReviewDate(Date drReviewDate) {
-		this.drReviewDate = drReviewDate;
+	public void setStatusChangeDate(GregHijDate statusChangeDate) {
+		this.statusChangeDate = statusChangeDate;
 	}
 
 	public String getCreatedBy() {
@@ -153,6 +75,5 @@ public class Arzi implements Serializable {
 	public void setUpdatedDate(Date updatedDate) {
 		this.updatedDate = updatedDate;
 	}
-
 
 }
