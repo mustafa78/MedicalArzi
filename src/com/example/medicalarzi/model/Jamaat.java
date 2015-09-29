557e17c60d6339866cd22ee2a889ddfde043b5c2
mkanchwa <mustafa.kanchwala@gmail.com>
2015-09-29 14:17:53 -0400

    New Jamaat model which has the jamaat to region & country mapping.

---
+++ src/com/example/medicalarzi/model/Jamaat.java
@@ -0,0 +1,115 @@
+/**
+ * 
+ */
+package com.example.medicalarzi.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.apache.commons.lang3.builder.RecursiveToStringStyle;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+
+/**
+ * @author mkanchwa
+ *
+ */
+public class Jamaat implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private Long jamaatId;
+
+	private String jamaatName;
+
+	private String region;
+
+	private String country;
+
+	private String createdBy;
+
+	private Date createdDate;
+
+	private String updatedBy;
+
+	private Date updatedDate;
+
+	/**
+	 * 
+	 */
+	public Jamaat() {
+	}
+
+	public Long getJamaatId() {
+		return jamaatId;
+	}
+
+	public void setJamaatId(Long jamaatId) {
+		this.jamaatId = jamaatId;
+	}
+
+	public String getJamaatName() {
+		return jamaatName;
+	}
+
+	public void setJamaatName(String jamaatName) {
+		this.jamaatName = jamaatName;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	public Date getUpdatedDate() {
+		return updatedDate;
+	}
+
+	public void setUpdatedDate(Date updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+
+	@Override
+	public String toString() {
+		return (new ReflectionToStringBuilder(this,
+				RecursiveToStringStyle.MULTI_LINE_STYLE)).toString();
+	}
+
+}
