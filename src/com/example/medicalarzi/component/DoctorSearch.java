1f96f4fc12b73011b99d6d8ddfcbb5f04290b3aa
mkanchwa <mustafa.kanchwala@gmail.com>
2015-12-30 01:24:26 -0500

    Implement the search component functionality to load the grid data with the search results on the button click. Bind the BeanItemContainer to the fields and load the new search period dropdown with data.

--- src/com/example/medicalarzi/component/DoctorSearch.java
+++ src/com/example/medicalarzi/component/DoctorSearch.java
@@ -1,107 +1,173 @@
 package com.example.medicalarzi.component;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.example.medicalarzi.model.ArziSearchCriteria;
 import com.example.medicalarzi.model.ArziSearchResult;
 import com.example.medicalarzi.model.ArziType;
 import com.example.medicalarzi.model.BodyPart;
 import com.example.medicalarzi.model.Condition;
+import com.example.medicalarzi.model.GregHijDate;
 import com.example.medicalarzi.model.Jamaat;
+import com.example.medicalarzi.model.Lookup;
 import com.example.medicalarzi.model.Procedure;
 import com.example.medicalarzi.service.ReviewerService;
 import com.example.medicalarzi.service.ServiceLocator;
 import com.example.medicalarzi.util.MedicalArziConstants;
 import com.example.medicalarzi.util.MedicalArziUtils;
+import com.vaadin.data.fieldgroup.BeanFieldGroup;
+import com.vaadin.data.fieldgroup.FieldGroup.CommitException;
+import com.vaadin.data.fieldgroup.PropertyId;
 import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.shared.Position;
 import com.vaadin.ui.AbstractSelect.ItemCaptionMode;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.Grid;
+import com.vaadin.ui.Notification.Type;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.VerticalSplitPanel;
+import com.vaadin.ui.renderers.DateRenderer;
 
-public class DoctorSearch extends CustomComponent {
+public class DoctorSearch extends CustomComponent implements ClickListener {
 
 	private static final long serialVersionUID = -2608386102633068228L;
-	
+
 	private static Logger logger = LogManager.getLogger(DoctorSearch.class);
-	
+
+	// Layout and components
 	private VerticalSplitPanel splitPanel;
 
-	private Grid arziGrid;
+	private Grid resultsGrid;
 
 	private VerticalLayout topLayout;
 
-	private Button search;
-	
-	
 	private CustomFormComponent searchCriteria;
-	
 
-	// Search criteria fields
+	private Button searchBtn;
+
+	// Search criteria fields bound to ArziSearchCriteria object.
+	@PropertyId("itsNumber")
+	private TextField itsNumber;
+
+	@PropertyId("firstName")
+	private TextField firstName;
+
+	@PropertyId("lastName")
+	private TextField lastName;
+
+	@PropertyId("arziType")
 	private ComboBox arziType;
-	
+
+	@PropertyId("bodyPart")
 	private ComboBox bodyPart;
 
+	@PropertyId("procedure")
 	private ComboBox procedure;
 
+	@PropertyId("condition")
 	private ComboBox condition;
 
+	@PropertyId("jamaat")
 	private ComboBox jamaat;
 	
+	@PropertyId("numberOfDays")
+	private ComboBox numOfDays;
+
+	// Binding Fields
+	private BeanFieldGroup<ArziSearchCriteria> searchCriteriaFieldsBinder;
+
+	// Grid container
+	private BeanItemContainer<ArziSearchResult> resultsContainer;
 
 	/**
-	 * The constructor should first build the main layout, set the
-	 * composition root and then do any custom initialization.
+	 * The constructor should first build the main layout, set the composition
+	 * root and then do any custom initialization.
 	 *
 	 */
 	public DoctorSearch() {
-		//Build the split panel
+		// Build the split panel. This is the mainLayout for this component
 		buildSplitPanel();
-		
-		//The root component. Must be set
+
+		// Bind the member fields to the model
+		bindFieldsToModel();
+
+		// The root component. Must be set
 		setCompositionRoot(splitPanel);
+	}
+
+	/**
+	 * This method is responsible for binding the member fields to the
+	 * ArziSearchCriteria model.
+	 */
+	private void bindFieldsToModel() {
+		logger.debug("Binding the patient and the arzi fields to their respective models.");
+
+		/* Bind the arzi fields. */
+		ArziSearchCriteria searchCriteriaObj = new ArziSearchCriteria();
+		/* Bind the search criteria fields */
+		searchCriteriaFieldsBinder = new BeanFieldGroup<ArziSearchCriteria>(
+				ArziSearchCriteria.class);
+		// Bind all the member fields whose type extends Field to the property
+		// id of the item.
+		// The mapping is done based on the @PropertyId annotation
+		searchCriteriaFieldsBinder.bindMemberFields(this);
+		// Add bean item that is bound.
+		searchCriteriaFieldsBinder.setItemDataSource(searchCriteriaObj);
+		// Set the buffered mode on, if using the bean validation JSR-3.0 with
+		// vaadin. Does not work when fields are immediately updated with the
+		// bean.
+		searchCriteriaFieldsBinder.setBuffered(true);
 	}
 
 	private void buildSplitPanel() {
 		// top-level component properties
 		setSizeFull();
-		
+
 		// common part: create layout
 		splitPanel = new VerticalSplitPanel();
 		splitPanel.setImmediate(false);
 		splitPanel.setLocked(true);
-		splitPanel.setHeight("400px");
-		splitPanel.setWidth("100%");		
+		splitPanel.setSizeFull();
+		splitPanel.setSplitPosition(65, Unit.PERCENTAGE, true);
 
 		// topLayout
 		buildTopLayout();
 		splitPanel.addComponent(topLayout);
-		
+
 		// arziTable
-		arziGrid = new Grid();
-		arziGrid.setImmediate(false);
-		arziGrid.setSizeFull();
-		splitPanel.addComponent(arziGrid);
+		buildResultsGrid();
+		splitPanel.addComponent(resultsGrid);
 	}
 
 	private void buildTopLayout() {
 		// common part: create layout
 		topLayout = new VerticalLayout();
 		topLayout.setImmediate(false);
-		topLayout.setMargin(false);
+		topLayout.setMargin(true);
+		topLayout.setSpacing(true);
 
-		// criteriaLayout
+		// searchCriteria
 		buildSearchCriteria();
 		topLayout.addComponent(searchCriteria);
 
 		// search
-		search = new Button("Search");
-		search.setImmediate(true);
-		topLayout.addComponent(search);
+		searchBtn = new Button("Search");
+		searchBtn.addClickListener(this);
+		topLayout.addComponent(searchBtn);
+		topLayout.setComponentAlignment(searchBtn, Alignment.MIDDLE_CENTER);
 	}
 
 	private void buildSearchCriteria() {
@@ -118,6 +184,18 @@ public class DoctorSearch extends CustomComponent {
 		FormLayout leftFormLayout = (FormLayout) MedicalArziUtils.findById(
 				searchCriteria,
 				MedicalArziConstants.CUSTOM_FORM_LEFTFORM_LAYOUT_ID);
+		// itsNumber
+		itsNumber = new TextField("ITS Number:");
+		itsNumber.setMaxLength(8);
+		itsNumber.setNullRepresentation("");
+		itsNumber.setConverter(MedicalArziUtils.itsNumberConverter());
+		leftFormLayout.addComponent(itsNumber);
+
+		// lastName
+		lastName = new TextField("Surname/Last Name:");
+		lastName.setNullRepresentation("");
+		lastName.setWidth("300px");
+		leftFormLayout.addComponent(lastName);
 
 		// region
 		loadJamaats();
@@ -126,10 +204,9 @@ public class DoctorSearch extends CustomComponent {
 		// bodyPart
 		loadBodyParts();
 		leftFormLayout.addComponent(bodyPart);
-
-		// procedure
-		loadProcedures();
-		leftFormLayout.addComponent(procedure);
+		
+		loadNumberOfDays();
+		leftFormLayout.addComponent(numOfDays);
 
 		/**
 		 * Add the fields to the right FormLayout.
@@ -139,6 +216,12 @@ public class DoctorSearch extends CustomComponent {
 				searchCriteria,
 				MedicalArziConstants.CUSTOM_FORM_RIGHTFORM_LAYOUT_ID);
 
+		// firstName
+		firstName = new TextField("First Name:");
+		firstName.setNullRepresentation("");
+		firstName.setWidth("300px");
+		rightFormLayout.addComponent(firstName);
+
 		// bodyPart
 		loadArziTypes();
 		rightFormLayout.addComponent(arziType);
@@ -147,8 +230,12 @@ public class DoctorSearch extends CustomComponent {
 		loadConditions();
 		rightFormLayout.addComponent(condition);
 
+		// procedure
+		loadProcedures();
+		rightFormLayout.addComponent(procedure);
+
 	}
-	
+
 	private void loadJamaats() {
 		jamaat = new ComboBox("Jamaat:");
 		jamaat.setImmediate(true);
@@ -210,89 +297,168 @@ public class DoctorSearch extends CustomComponent {
 		procedure.setItemCaptionPropertyId("procedureName");
 		procedure.setRequired(false);
 	}
-
-	private void initArziList() {
-		/*arziGrid.setContainerDataSource(myArziContainer);
-		arziGrid.setSelectable(true);
-		arziGrid.setImmediate(true);
-		arziGrid.setColumnHeaderMode(ColumnHeaderMode.EXPLICIT);
-		arziGrid.setVisibleColumns(new Object[]{"arziId", "arziCategory", "region", "country", "status"});
-		arziGrid.setColumnHeaders(new String[]{"ID", "CATEGORY", "REGION", "COUNTRY", "STATUS"});*/
+	
+	private void loadNumberOfDays() {
+		numOfDays = new ComboBox("Search Period:");
+		numOfDays.setContainerDataSource(MedicalArziUtils
+				.getContainer(Lookup.class));
+		numOfDays.addItems(ServiceLocator.getInstance().getLookupService()
+				.getByLookupType(MedicalArziConstants.SEARCH_NUM_OF_DAYS));
+		numOfDays.setInputPrompt("Please select the number of days to search.");
+		numOfDays.setItemCaptionMode(ItemCaptionMode.PROPERTY);
+		numOfDays.setItemCaptionPropertyId("lookupValue");
+		numOfDays.setRequired(false);
 	}
 
+	@SuppressWarnings("unchecked")
 	private void buildResultsGrid() {
-		@SuppressWarnings("unchecked")
-		BeanItemContainer<ArziSearchResult> resultsContainer = (BeanItemContainer<ArziSearchResult>) MedicalArziUtils
+		resultsGrid = new Grid();
+		resultsGrid.setSizeFull();
+		resultsGrid.setImmediate(false);
+
+		resultsContainer = (BeanItemContainer<ArziSearchResult>) MedicalArziUtils
 				.getContainer(ArziSearchResult.class);
+
+		// Set the container as the grid datasource
+		resultsGrid.setContainerDataSource(resultsContainer);
 		
-		ReviewerService service = ServiceLocator.getInstance().getReviewerService();
-		
-		//service.searchArzisByCriteria(criteria);
+		// Add nested properties to the header
+		resultsContainer.addNestedContainerBean("patient");
+		resultsContainer.addNestedContainerBean("arzi");
 
-		 /*ArrayList<Arzi> arziList = new ArrayList<Arzi>();
-		 for (int i = 1; i < 50; i++) {
-			 Arzi arzi = new Arzi();
-			 Status status = new Status();
-			 arzi.setArziId(new Long((long)(i* Math.random() + 1000)));
-			 if(i%2 == 0)
-				 arzi.setArziCategory("Doa");
-			 else
-				 arzi.setArziCategory("Raza");
+		// Add all the intermediate nested properties (which are also
+		// beans)
+		resultsContainer.addNestedContainerBean("patient.jamaat");
+		resultsContainer.addNestedContainerBean("arzi.currentStatus");
+		resultsContainer.addNestedContainerBean("arzi.arziType");
+		resultsContainer.addNestedContainerBean("arzi.bodyPart");
+		resultsContainer.addNestedContainerBean("arzi.condition");
+		resultsContainer
+				.addNestedContainerBean("arzi.conditionStartDate");
+		resultsContainer.addNestedContainerBean("arzi.procedure");
 
-			 if(i%3 == 0) {
-				 status.setStatusDesc("Inprocess");
-				 arzi.setStatus(status);
-			 }
-			 else if(i%7 == 0) {
-				 status.setStatusDesc("Completed");
-				 arzi.setStatus(status);
-			 }
-			 else if(i%5 == 0) {
-				 status.setStatusDesc("Waiting");
-				 arzi.setStatus(status);
-			 }
-			 else {
-				 status.setStatusDesc("Submitted");
-				 arzi.setStatus(status);
-			 }
+		//Setting the grid columns
+		resultsGrid.setColumns("patient.itsNumber",
+				"patient.firstName", "patient.lastName",
+				"patient.jamaat.jamaatName",
+				"patient.jamaat.jamiyatName",
+				"arzi.currentStatus.statusDesc",
+				"arzi.arziType.arziTypeName",
+				"arzi.bodyPart.bodyPartName",
+				"arzi.condition.conditionName", "arzi.otherCondition",
+				"arzi.conditionStartDate.gregorianCalDate",
+				"arzi.procedure.procedureName");
 
-			 long randomNum = (long)(i* Math.random() + 1000);
-			 if(randomNum % 2 == 0) {
-				 arzi.setRegion("North America");
-				 arzi.setCountry("USA");
-			 }
-			 else if(randomNum % 3 == 0) {
-				 arzi.setRegion("North America");
-				 arzi.setCountry("Canada");
-			 }
-			 if(randomNum % 5 == 0) {
-				 arzi.setRegion("Asia");
-				 arzi.setCountry("India");
-			 }
-			 if(randomNum % 7 == 0) {
-				 arzi.setRegion("Asia");
-				 arzi.setCountry("Pakistan");
-			 }
-			 if(randomNum % 9 == 0) {
-				 arzi.setRegion("Europe");
-				 arzi.setCountry("United Kingdom");
-			 }
-			 if(randomNum % 2 == 1) {
-				 arzi.setRegion("Europe");
-				 arzi.setCountry("France");
-			 }
-			 if(randomNum % 3 == 1) {
-				 arzi.setRegion("Africa");
-				 arzi.setCountry("Kenya");
-			 }
-			 if(randomNum % 5 == 1) {
-				 arzi.setRegion("Africa");
-				 arzi.setCountry("Tanzania");
-			 }
-			 arziList.add(arzi);
-		 }
+		//Customize the grid columns
+		customizeResultsGridColumns();		
+	}
+	
+	/**
+	 * This method is responsible for customizing all the grid columns like
+	 * setting the caption, setting the editor fields and grouping the similar
+	 * columns together etc.
+	 * 
+	 */
+	private void customizeResultsGridColumns() {
+		// Sets the converter on the ITS number to remove the grouping used.
+		resultsGrid.getColumn("patient.itsNumber").setConverter(
+				MedicalArziUtils.itsNumberConverter());
 
-		 bc.addAll(arziList);*/
+		resultsGrid.getColumn("arzi.currentStatus.statusDesc")
+				.setHeaderCaption("Arzi Status");
+
+		resultsGrid
+				.getColumn("arzi.conditionStartDate.gregorianCalDate")
+				.setHeaderCaption("Condition Start Date")
+				.setRenderer(
+						new DateRenderer("%1$tB %1$td, %1$tY", Locale.ENGLISH));
+
 	}	
+
+	/**
+	 * Implement the button click listener. Called when the 'Search' button is
+	 * clicked.
+	 * 
+	 * @param event
+	 */
+	@Override
+	public void buttonClick(ClickEvent event) {
+
+		if (event.getButton().equals(searchBtn)) {
+			try {
+				// FieldGroup buffered mode is on, so commit() is required.
+				// Throws CommitException
+				searchCriteriaFieldsBinder.commit();
+
+				ArziSearchCriteria userEnteredSearchCriteria = searchCriteriaFieldsBinder
+						.getItemDataSource().getBean();
+
+				ReviewerService reviewerService = ServiceLocator.getInstance()
+						.getReviewerService();
+				
+				Lookup searchPeriod = (Lookup)numOfDays.getValue();
+				
+				//If the user entered the number of days to search as one of the search criteria
+				if (searchPeriod != null) {
+
+					GregHijDate currentDate = ServiceLocator.getInstance()
+							.getLookupService()
+							.getRequestedGregorianHijriCalendar(new Date());
+
+					userEnteredSearchCriteria.setCurrentDate(currentDate);
+
+					// this would default to now
+					Calendar calendar = Calendar.getInstance();
+
+					// Subtract the number of days to get the
+					if (searchPeriod.getLookupId().intValue() == MedicalArziConstants.SEARCH_PERIOD_LAST_7_DAYS_ID) {
+						calendar.add(Calendar.DAY_OF_MONTH, -7);
+					} else if (searchPeriod.getLookupId().intValue() == MedicalArziConstants.SEARCH_PERIOD_LAST_10_DAYS_ID) {
+						calendar.add(Calendar.DAY_OF_MONTH, -10);
+					} else if (searchPeriod.getLookupId().intValue() == MedicalArziConstants.SEARCH_PERIOD_LAST_14_DAYS_ID) {
+						calendar.add(Calendar.DAY_OF_MONTH, -14);
+					} else if (searchPeriod.getLookupId().intValue() == MedicalArziConstants.SEARCH_PERIOD_LAST_21_DAYS_ID) {
+						calendar.add(Calendar.DAY_OF_MONTH, -21);
+					} else if (searchPeriod.getLookupId().intValue() == MedicalArziConstants.SEARCH_PERIOD_LAST_30_DAYS_ID) {
+						calendar.add(Calendar.DAY_OF_MONTH, -30);
+					}
+
+					GregHijDate dateForSearchPeriod = ServiceLocator
+							.getInstance()
+							.getLookupService()
+							.getRequestedGregorianHijriCalendar(
+									calendar.getTime());
+
+					userEnteredSearchCriteria
+							.setSearchPeriodDate(dateForSearchPeriod);
+				}
+
+				List<ArziSearchResult> searchResultList = reviewerService
+						.searchArzisByCriteria(userEnteredSearchCriteria);
+
+				/**
+				 * Update the Grid with fresh data. Two step process of replacing bean
+				 * items. (1) First remove all BeanItem objects with
+				 * Container::removeAllItems method. (2) Then add replacement BeanItem
+				 * objects with the BeanItemContainer::addAll method.
+				 */				
+				resultsContainer.removeAllItems();
+				// Add data to the container
+				resultsContainer.addAll(searchResultList);
+
+			} catch (CommitException ce) {
+				logger.error(ce);
+
+				String errorDescription = "Please fix the errors before proceeding further.";
+
+				// Create an error notification if the required fields are not
+				// entered correctly.
+				MedicalArziUtils.createAndShowNotification(null,
+						errorDescription, Type.ERROR_MESSAGE,
+						Position.TOP_LEFT, "errorMsg", -1);
+			}
+		}
+
+	}
 
 }
